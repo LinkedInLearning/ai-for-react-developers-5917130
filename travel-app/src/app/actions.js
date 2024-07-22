46bb9f64214e250872e1c425bd288dddd8386796
eveporcello <eve@moonhighway.com>
2024-07-22 14:45:30 -0400

    Adds 03_05

--- travel-app/src/app/actions.js
+++ travel-app/src/app/actions.js
@@ -1,38 +1,14 @@
 "use server";
 
-import { streamObject } from "ai";
+import { streamUI } from "ai";
 import { openai } from "@ai-sdk/openai";
-import { createStreamableValue } from "ai/rsc";
-import { z } from "zod";
 
-export async function generate(input) {
-  "use server";
-
-  const stream = createStreamableValue();
-
-  (async () => {
-    const { partialObjectStream } = await streamObject({
-      model: openai("gpt-4-turbo"),
-      system: "You generate fake data for three people",
-      prompt: input,
-      schema: z.object({
-        people: z.array(
-          z.object({
-            name: z
-              .string()
-              .describe("name of a fake person"),
-            address: z
-              .string()
-              .describe("US address format"),
-            age: z.number()
-          })
-        )
-      })
-    });
-    for await (const partialObject of partialObjectStream) {
-      stream.update(partialObject);
-    }
-    stream.done();
-  })();
-  return { object: stream.value };
+export async function streamComponent() {
+  const result = await streamUI({
+    model: openai("gpt-4o"),
+    prompt:
+      "Give me some advice on how to plan a trip to Telluride, Colorado",
+    text: ({ content }) => <div>{content}</div>
+  });
+  return result.value;
 }
