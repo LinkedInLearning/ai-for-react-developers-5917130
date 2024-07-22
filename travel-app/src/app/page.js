10ed8e14d1d42601f91adb47165f7de203a3d1d1
eveporcello <eve@moonhighway.com>
2024-07-22 11:34:24 -0400

    Adds 01_05b

--- travel-app/src/app/page.js
+++ travel-app/src/app/page.js
@@ -1,7 +1,28 @@
+"use client";
+
+import { useCompletion } from "ai/react";
+
 export default function Home() {
+  const {
+    completion,
+    input,
+    handleInputChange,
+    handleSubmit
+  } = useCompletion();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Travel App Powered by Vercel AI</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={input}
+          placeholder="What to do where"
+          onChange={handleInputChange}
+        />
+      </form>
+      {completion ? (
+        <div>{completion}</div>
+      ) : (
+        <div>Recommend places to visit...</div>
+      )}
     </main>
   );
 }
