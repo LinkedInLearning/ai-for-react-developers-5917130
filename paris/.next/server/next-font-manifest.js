fa4dc1b3e3304d132a37696c08411932dcfead89
eveporcello <eve@moonhighway.com>
2024-09-04 08:02:23 -0700

    Adds folder

--- paris/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/eveporcello/Desktop/ExerciseFiles/Ch04/04_04/04_04e/paris/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- paris/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"N3jqrkmSG6hEspvJk7rgbmMkuM4CF4vljReNqhUjS9w=\"\n}"
