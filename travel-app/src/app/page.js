5f7b5538f5141665ab34cd053ba2f6690bd2aaa3
eveporcello <eve@moonhighway.com>
2024-09-04 06:47:12 -0700

    Adds 01_05b

---
+++ travel-app/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Travel App Powered by Vercel AI</h1>
+    </main>
+  );
+}
