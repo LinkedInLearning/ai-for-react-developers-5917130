6e59f32cb470850a3f37b6d1c5f79a0f3ea1fb8c
eveporcello <eve@moonhighway.com>
2024-09-04 07:52:05 -0700

    Adds paris directory

---
+++ paris/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"N3jqrkmSG6hEspvJk7rgbmMkuM4CF4vljReNqhUjS9w=\"\n}"
---
+++ paris/src/app/page.js
@@ -0,0 +1,9 @@
+import { ParisPage } from "../components/paris-page.jsx";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ParisPage />
+    </main>
+  );
+}
